2edd041bc0e5c571294d79631b857a9c4f36c374
AryanPanda <aryanpanda100@gmail.com>
2023-09-23 17:18:09 +0530

    adding cross origin

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const errorMiddleware = require("./middleware/error");
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(cors());
 
 // Route Imports
 const product = require("./routes/productRoute");
