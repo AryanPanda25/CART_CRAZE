11468abfd8023c7543ed6067ea43c28eb83c4cbc
AryanPanda <aryanpanda100@gmail.com>
2023-09-25 20:54:18 +0530

    Resovlve

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(cors({
-    origin: "*"
+    origin: "https://cart-craze.vercel.app"
 }));
 
 // Route Imports
